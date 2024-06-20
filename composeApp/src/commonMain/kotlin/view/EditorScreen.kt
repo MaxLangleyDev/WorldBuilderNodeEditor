6c4c11f4671d1fce5929b5635fd19fae0eef91d8
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-20 10:44:56 -0700

    Create EditorScreen.kt

---
+++ composeApp/src/commonMain/kotlin/view/EditorScreen.kt
@@ -0,0 +1,32 @@
+package view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import view.map.MapView
+import viewmodel.MapEditorViewModel
+
+@Composable
+fun EditorScreen(
+    modifier: Modifier = Modifier
+){
+    val viewModel = viewModel { MapEditorViewModel() }
+    val state by viewModel.state.collectAsState()
+
+    Column(
+        modifier = modifier
+    ){
+        Row(
+            modifier.weight(0.7f).fillMaxWidth()
+        ){
+            MapView(map = state.map)
+        }
+
+    }
+    
+}
