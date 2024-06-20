09eff701ce94edf857ecbfef413cc590bb225dd8
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-19 21:25:25 -0700

    Create MapNode.kt

---
+++ composeApp/src/commonMain/kotlin/model/MapNode.kt
@@ -0,0 +1,10 @@
+package model
+
+import androidx.compose.ui.graphics.Color
+
+class MapNode(
+    val name: String = "",
+    val traversable: Boolean = false,
+    val color: Color = Color.White
+) {
+}
