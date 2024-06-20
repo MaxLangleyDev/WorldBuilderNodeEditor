630e54595dbef99efd8ee644f8807c38622e04df
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-20 10:15:16 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,26 +1,14 @@
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import model.GameMap
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import presentation.MapViewer
+import presentation.EditorScreen
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
 
-        val map = GameMap()
-
-        MapViewer(map = map)
-
+        EditorScreen()
 
     }
 }
