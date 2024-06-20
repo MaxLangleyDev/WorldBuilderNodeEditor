70466f8e9a3b5d0bb2e573aa3425edd933580328
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-20 10:45:07 -0700

    Update App.kt

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,14 +1,22 @@
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import presentation.EditorScreen
+import view.EditorScreen
+import view.map.MapView
+import theme.MapEditorTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    MapEditorTheme {
 
-        EditorScreen()
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
+            EditorScreen(modifier = Modifier.fillMaxSize())
+
+        }
     }
 }
