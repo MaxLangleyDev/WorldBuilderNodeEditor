985533cfd814d7afc3cd183832f3d67aa0dd3b57
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-21 20:40:37 -0700

    Update MapView.kt

--- composeApp/src/commonMain/kotlin/view/map/MapView.kt
+++ composeApp/src/commonMain/kotlin/view/map/MapView.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,17 +27,27 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.dp
 import model.GameMap
+import model.MapEditorState
+import model.MapNode
 
 @Composable
 fun MapView(
     modifier: Modifier = Modifier,
-    map: GameMap = GameMap()
+    mapEditorState: MapEditorState = MapEditorState(),
 ) {
+//    val horizontalScrollState = rememberScrollState()
+//    val verticalScrollState = rememberScrollState()
+
     var isDragging by remember { mutableStateOf(false) }
     var brushSize by remember { mutableStateOf(32f) }
     var currentDragPosition by remember { mutableStateOf(Offset.Zero) }
+
+    var selectedIndices = remember { mutableStateListOf<Pair<Int, Int>>() }
+    val nodePositions = remember { mutableStateListOf<Pair< Pair<Int, Int>, Rect >>() }
 
     Box(
         modifier = modifier
@@ -43,8 +55,8 @@ fun MapView(
         Column(
             modifier =
             Modifier
-                .verticalScroll(rememberScrollState())
-                .horizontalScroll(rememberScrollState())
+//                .verticalScroll(verticalScrollState)
+//                .horizontalScroll(horizontalScrollState)
                 .pointerInput(Unit) {
                     detectDragGestures(
                         onDragStart = { offset ->
@@ -59,7 +71,11 @@ fun MapView(
                         },
                         onDrag = { change, _ ->
                             if (isDragging) {// Update selected nodes based on current pointer position
-                                // Calculate the brush area
+//                                val verticalScrollOffset = verticalScrollState.value.toFloat()
+//                                val horizontalScrollOffset = horizontalScrollState.value.toFloat()
+
+                                selectedIndices.clear()
+
                                 currentDragPosition = change.position
                                 val brushTopLeft =
                                     currentDragPosition - Offset(brushSize / 2, brushSize / 2)
@@ -71,10 +87,11 @@ fun MapView(
                                     bottom = brushBottomRight.y
                                 )
 
-                                // Update selected nodes based on the brush area
-                                //                            selectedNodes = nodes.filter { node ->
-                                //                                brushArea.contains(node.position)
-                                //                            }
+                                selectedIndices.addAll(
+                                    nodePositions.filter { (_, rect) ->
+                                        brushArea.overlaps(rect)
+                                    }.map { it.first }
+                                )
                             }
                         }
                     )
@@ -83,13 +100,29 @@ fun MapView(
         ) {
 
             // Draw the map nodes
-            for (x in 0..<map.sizeX) {
+            for (x in 0..<mapEditorState.map.sizeX) {
                 Row {
-                    for (y in 0..<map.sizeY) {
+                    for (y in 0..<mapEditorState.map.sizeY) {
                         Column(
                             modifier = Modifier.padding(3.dp)
                         ) {
-                            MapNodeView(mapNode = map.nodes[x][y])
+                            MapNodeView(
+                                modifier = Modifier
+                                    .size(30.dp)
+                                    .onGloballyPositioned {
+                                            coordinates ->
+                                        val rect = Rect(
+                                            coordinates.positionInRoot().x,
+                                            coordinates.positionInRoot().y,
+                                            coordinates.positionInRoot().x + coordinates.size.width,
+                                            coordinates.positionInRoot().y + coordinates.size.height
+                                        )
+
+                                        nodePositions.add((x to y) to rect)
+                                    }
+                                    .background(if ((Pair(x, y)) in selectedIndices) Color.Red else Color.Blue),
+                                mapNode = mapEditorState.map.nodes[x][y]
+                            )
                         }
                     }
                 }
@@ -98,10 +131,14 @@ fun MapView(
 
         // Render the brush rectangle if dragging
         if (isDragging) {
+//            val verticalScrollOffset = verticalScrollState.value.toFloat()
+//            val horizontalScrollOffset = horizontalScrollState.value.toFloat()
+
             Canvas(modifier = Modifier.fillMaxSize()) {
                 drawRect(
                     color = Color.Gray.copy(alpha = 0.5f),
-                    topLeft = currentDragPosition - Offset(brushSize / 2, brushSize / 2),
+                    topLeft = currentDragPosition
+                            - Offset(brushSize / 2, brushSize / 2),
                     size = Size(brushSize, brushSize),
                     style = Stroke(width = 2.dp.toPx())
                 )
