cfd1a3c44a669c818076bccb6f7169724566339d
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-26 10:26:15 -0700

    Update GameMap.kt

--- composeApp/src/commonMain/kotlin/model/GameMap.kt
+++ composeApp/src/commonMain/kotlin/model/GameMap.kt
@@ -4,11 +4,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GameMap (
-    val sizeX: Int = 24,
+    val sizeX: Int = 36,
     val sizeY: Int = 24,
     val nodes : List<List<MapNode>> =
-        List(sizeX) { x ->
-            List(sizeY) { y ->
+        List(sizeY) { y ->
+            List(sizeX) { x ->
                 MapNode(x = x, y = y)
             }
         }
