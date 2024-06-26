df4291d5cf8f1517ee5e2b58b0adec5b83900963
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-26 10:03:47 -0700

    Update MapNode.kt

--- composeApp/src/commonMain/kotlin/model/MapNode.kt
+++ composeApp/src/commonMain/kotlin/model/MapNode.kt
@@ -5,8 +5,22 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MapNode(
     val name: String = "",
+
     val x: Int = 0,
     val y: Int = 0,
-    val traversable: Boolean = false,
+
+    val traversableFrom: Map<String, Boolean> = mapOf(
+        "n" to true,
+        "s" to true,
+        "e" to true,
+        "w" to true,
+    ),
+    val traversableTo: Map<String, Boolean> = mapOf(
+        "n" to true,
+        "s" to true,
+        "e" to true,
+        "w" to true,
+    ),
+
     val colorHex: Int = 0xFF000000.toInt(),
 )
