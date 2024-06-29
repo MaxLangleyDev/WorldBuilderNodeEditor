ff0a878b3a372f83f1b3b3efd7234c826b89197a
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-28 19:53:10 -0700

    Update MapNodeView.kt

--- composeApp/src/commonMain/kotlin/view/map/MapNodeView.kt
+++ composeApp/src/commonMain/kotlin/view/map/MapNodeView.kt
@@ -1,13 +1,11 @@
 package view.map
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import model.MapNode
+import model.gameMap.MapNode
 
 @Composable
 fun MapNodeView(
