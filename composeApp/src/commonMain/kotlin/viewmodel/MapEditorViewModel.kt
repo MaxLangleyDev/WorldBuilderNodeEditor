7f9a0a557b250c883671d5db4ff65a26da1ba90f
MaxLangleyIV <32246043+MaxLangleyDev@users.noreply.github.com>
2024-06-28 19:53:13 -0700

    Update MapEditorViewModel.kt

--- composeApp/src/commonMain/kotlin/viewmodel/MapEditorViewModel.kt
+++ composeApp/src/commonMain/kotlin/viewmodel/MapEditorViewModel.kt
@@ -3,7 +3,9 @@ package viewmodel
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import model.MapEditorState
+import model.gameMap.MapNode
 
 class MapEditorViewModel(
     initialState: MapEditorState = MapEditorState()
@@ -11,5 +13,16 @@ class MapEditorViewModel(
 
     private val _state = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
+
+    fun addSelectedNode(node: MapNode){
+
+        val selectedNodes = _state.value.selectedNodes?.toMutableList() ?: mutableListOf()
+        selectedNodes.add(node)
+        _state.update {
+            it.copy(selectedNodes = selectedNodes.toSet().toList())
+        }
+        _state.value = _state.value.copy(selectedNodes = selectedNodes)
+
+    }
 
 }
